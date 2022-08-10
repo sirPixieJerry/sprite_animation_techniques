37287bfbaf989eab5eff68a5ba0d8b2b38e08b4e
CryptedJerry <jerry.bucquet@gmail.com>
2022-08-10 17:04:30 +0200

    sprite animator finished. next: refactor code for reuse.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+let playerState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function (e) {
+    playerState = e.target.value;
+});
 const canvas = document.getElementById("canvas-one");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 600);
@@ -5,24 +10,93 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 
 // console.log(ctx); // -> logs the context object and all methods!
 
-// animate sprites - method one:
+const playerImage = new Image();
+playerImage.src = "../image/shadow_dog.png";
+const spriteWidth = 575; // sprite width (px) divided through columns
+const spriteHeight = 523; // sprite height (px) divided through rows
 
-const plrImg = new Image();
-plrImg.src = "../image/shadow_dog.png";
-const sprW = 575; // sprite width (px) divided through columns
-const sprH = 523; // sprite height (px) divided through rows
-let frX = 0;
-let frY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "idle",
+        frames: 7,
+    },
+    {
+        name: "jump",
+        frames: 7,
+    },
+    {
+        name: "fall",
+        frames: 7,
+    },
+    {
+        name: "run",
+        frames: 9,
+    },
+    {
+        name: "dizzy",
+        frames: 11,
+    },
+    {
+        name: "sit",
+        frames: 5,
+    },
+    {
+        name: "roll",
+        frames: 7,
+    },
+    {
+        name: "bite",
+        frames: 7,
+    },
+    {
+        name: "ko",
+        frames: 12,
+    },
+    {
+        name: "getHit",
+        frames: 4,
+    },
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    };
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+
+// animate sprite - method two (advanced):
 
 function animateSprite() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillStyle = "whitesmoke";
-    ctx.fillRect(0, 0, 600, 600);
-    // .drawImage can take up to 9 arguments:
-    // the image src: .drawImage(image)
-    // and the cutout: .drawImage(image, sx, sy, sw, sh)
-    // and the render destination: .drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
-    ctx.drawImage(plrImg, frX * sprW, frY * sprW, sprW, sprH, 0, 0, sprW, sprH);
+    // ctx.fillStyle = "whitesmoke";
+    // ctx.fillRect(0, 0, 600, 600);
+    let position =
+        Math.floor(gameFrame / staggerFrames) %
+        spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(
+        playerImage,
+        frameX,
+        frameY,
+        spriteWidth,
+        spriteHeight,
+        0,
+        0,
+        spriteWidth,
+        spriteHeight
+    );
+    gameFrame++;
     requestAnimationFrame(animateSprite);
 }
 
